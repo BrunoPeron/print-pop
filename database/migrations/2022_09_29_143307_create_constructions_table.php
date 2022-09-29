06e1817c6714dfa3b7dd0ab20b34e782ae7b8bac
bruno <prog44@ixcsoft.com>
2022-09-29 11:57:31 -0300

    feat(database): implementaded database of application

---
+++ database/migrations/2022_09_29_143307_create_constructions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('constructions', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', '50');
+            $table->unsignedBigInteger('id_product');
+            $table->foreign('id_product')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('constructions');
+    }
+};
