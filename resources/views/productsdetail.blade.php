c46b9d9baca9240ac7b17ee8dfdfa20f283ac132
bruno <prog44@ixcsoft.com>
2022-09-30 17:17:21 -0300

    feat(database): implementaded product detail

--- resources/views/productsdetail.blade.php
+++ resources/views/productsdetail.blade.php
@@ -109,6 +109,7 @@
 <!-- Related items section-->
 <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
+        <h2 class="fw-bolder mb-4">Produtos relacionados</h2>
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
             <div class="col mb-5">
                 <div class="card h-100" style="background-color: #9B5A44">
